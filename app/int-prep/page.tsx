d92c82f5cb2512b8278f5aef09bab48b5473a62d
Ramzyjof <ramzyjofvansah2000@gmail.com>
2026-07-03 14:30:58 +0700

    Add sitemap and robots.txt for SEO

--- app/int-prep/page.tsx
+++ app/int-prep/page.tsx
@@ -1,5 +1,29 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function Page() {
-  redirect("/services/int-prep");
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function IntPrepRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/services/int-prep");
+  }, [router]);
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#020617",
+        color: "#fff",
+        fontFamily: "inherit",
+        fontSize: "18px",
+      }}
+    >
+      Redirecting...
+    </div>
+  );
 }
